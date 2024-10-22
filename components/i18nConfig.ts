b38b1dc0528d795a400ca0faaef3444c95a8a888
YouMingYeh <b10705052@ntu.edu.tw>
2024-10-22 15:17:52 +0800

    Update i18nConfig for zh_TW locale support

--- components/i18nConfig.ts
+++ components/i18nConfig.ts
@@ -1,11 +1,11 @@
 export const i18nConfig = {
-    locales: ['en', 'zh_CN'],
+    locales: ['en', 'zh_TW'],
     defaultLocale: 'en'
 };
 
 const languages = [
     {locale: 'en', name: "English"},
-    {locale: 'zh_CN', name: "中文简体"}
+    {locale: 'zh_TW', name: "繁體中文"}
 ]
 export default languages;
 
